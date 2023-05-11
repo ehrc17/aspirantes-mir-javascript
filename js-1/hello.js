a4b89eb2936d31c4afbb0ee6992a4aa57608e98f
ehrc17 <68665461+ehrc17@users.noreply.github.com>
2023-05-10 19:42:37 -0500

    Update hello.js

--- js-1/hello.js
+++ js-1/hello.js
@@ -1,7 +1,7 @@
-// escribe la función hola acá
+function saludo (nombre) {
+    console.log("hola" + nombre)
+}
 
 // código de prueba
-console.log(hola("Pedro")) // "Hola Pedro!"
-console.log(hola("Juan")) // "Hola Juan!"
-console.log(hola("")) // "Hola !"
+saludo("Pedro") // "Hola Pedro!"
 
